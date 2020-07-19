9174a9bd7e286833811bd93f7f70809374576275
Sang Pham <sangpdh@imt-soft.com>
2020-07-19 20:44:49 +0700

    + Tablet: Delegate group screen.

--- src/containers/home/delegate/delegateGroup/delegateGroup.container.tsx
+++ src/containers/home/delegate/delegateGroup/delegateGroup.container.tsx
@@ -3,6 +3,10 @@ import { NavigationInjectedProps } from 'react-navigation';
 import { DelegateGroup } from './delegateGroup.component';
 import { User as UserModel } from '@src/core/models/user/user.model';
 import { delegateGroupDataFake } from '@src/core/data/delegateGroup';
+import { delegateListDataFake2 } from '@src/core/data/delegateList';
+import { DelegateGroupTablet } from './delegateGroup.component.tablet';
+import { isTablet } from 'react-native-device-info';
+import { KEY_NAVIGATION_BACK } from '@src/core/navigation/constants';
 
 export const DelegateGroupContainer: React.FunctionComponent<NavigationInjectedProps> = (props) => {
   const navigationKey: string = 'DelegateGroupContainer';
@@ -16,6 +20,20 @@ export const DelegateGroupContainer: React.FunctionComponent<NavigationInjectedP
       },
     });
   };
+
+  const onBackPress = (): void => {
+    props.navigation.goBack(KEY_NAVIGATION_BACK);
+  };
+
+  if (isTablet()) {
+    return (
+      <DelegateGroupTablet
+        delegateList={delegateListDataFake2}
+        onDelegateItemPress={onDelegateItemPress}
+        onBackPress={onBackPress}
+      />
+    );
+  }
 
   return (
     <DelegateGroup

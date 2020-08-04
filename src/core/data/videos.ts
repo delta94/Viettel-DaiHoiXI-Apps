5db478acf0aebbe6ddbf550c3801660cd30906a6
Tam Tran <tamtct@imt-soft.com>
2020-08-04 09:14:32 +0700

    - change videos source

--- src/core/data/videos.ts
+++ src/core/data/videos.ts
@@ -6,31 +6,26 @@ export const galleryVideoDataFake: Videos[] = [
     videos: [
       {
         id: 1,
-        name: 'Tập 1_Sợi chỉ đỏ',
-        url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+        name: 'Phát biểu bế mạc kỳ họp thứ 11 Quốc hội Khóa 13 của chủ tịch Quốc hội Nguyễn Thị Kim Ngân',
+        url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/Ky_Hop_QH/Kyho11/S12.4.16 phatbieubemac nguyen thi kim ngan.mp4',
       },
       {
         id: 2,
-        name: 'Tập 2_Xây dụng và bảo vệ',
-        url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/HINH DAI BIEU NGUYEN THANH XUAN.mp4',
+        name: 'BẠO LỰC GIA ĐÌNH: IM LẶNG LÀ TIẾP TAY CHO BẠO LỰC',
+        url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/LANHUONG/BAO LUC GIA DINH.mp4',
       },
       {
         id: 3,
-        name: 'Tập 3_Đổi mới và hội nhập',
-        url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/HINH DAI BIEU NGUYEN THANH XUAN.mp4',
+        name: 'Quốc hội nghe các báo cáo công tác nhiệm kỳ 2011-2016 của Chánh án Tòa án nhân dân tối cao, Viện trưởng Viện kiểm sát nhân dân tối cao và Tổng kiểm toán Nhà nước',
+        url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/Ky_Hop_QH/Kyho11/Chieu22.3.16P1.mp4',
       },
     ],
   },
   {
-    topic: 'Thành phố 40 năm xây dựng và phát triển',
+    topic: 'Ngày lễ hưởng ứng pháp luật Việt Nam',
     videos: [
       {
         id: 4,
-        name: 'Tập 1_Sợi chỉ đỏ',
-        url: 'https://www.w3schools.com/html/mov_bbb.mp4',
-      },
-      {
-        id: 5,
         name: 'PHÁT BIỂU CỦA CHỦ TỊCH QUỐC HỘI NGUYỄN THỊ KIM NGÂN TẠI LỄ HƯỞNG ỨNG NGÀY PHÁP LUẬT VIỆT NAM',
         url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/Le Thuong/PHAT BIEU CUA CTQH NGUYEN THI KIM NGAN TAI LE KY NIEM NGAY PHAP LUAT VIET NAM.mp4',
       },
@@ -40,12 +35,12 @@ export const galleryVideoDataFake: Videos[] = [
     topic: 'Phim tư liệu cập nhật tại đại hội đảng lần thứ 10',
     videos: [
       {
-        id: 6,
+        id: 5,
         name: ' ĐẠI BIỂU NGUYỄN THANH XUÂN CHẤT VẤN VỀ VIỆC TRIỂN KHAI NGHỊ QUYẾT 120',
         url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/HINH DAI BIEU NGUYEN THANH XUAN.mp4',
       },
       {
-        id: 7,
+        id: 6,
         name: 'Đại biểu Nguyễn Thanh Hiền chất vấn về an toàn hồ đập',
         url: 'http://demo.etv.quochoi.vn/Portals/0/VOD/LE PHUONG/HINH DAI BIEU NGUYEN THANH HIEN.mp4',
       },
